a33b5a01713b022b12c4669042906ed816761c5d
gokul <gokulalakshmi2022@gmail.com>
2023-08-06 22:46:00 +0530

    new java file

--- Add.java
+++ Add.java
@@ -1,6 +1,9 @@
-import java.util.*;
-class Add
+public class Add
 {
-    int a;
-    
+public static void main(String args[])
+{
+int n1 = 225, n2 = 115, sum;
+sum = n1 + n2;
+System.out.println("The sum of numbers is: "+sum);
+}
 }
